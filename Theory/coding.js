fbd43a3a632811a4af1cad0b3bba0502392e04d2
WiiYi <56102072+WiiYi@users.noreply.github.com>
2019-11-01 22:53:02 -0600

    Add files via upload

---
+++ Theory/coding.js
@@ -0,0 +1,37 @@
+function display2_1() {
+  var myanswer  = "My answer goes here: The first console.log will give undefined, the second will give true and the third will give false. For the second console.log it is comparing the value of null and undefined, the third console.log it is also comparing the type and value." // TODO 2.1: Fill in Answer here after the :
+  document.getElementById("21answer").innerHTML = myanswer; // Do not change
+}
+function display2_2() {
+  var myanswer  = "My answer goes here: It will give an error that x is not defined. If remove the use strict it will run as normal. Because the use strict will not all using undefined variables." // TODO 2.2: Fill in Answer here
+  document.getElementById("22answer").innerHTML = myanswer; // Do not change
+}
+
+function display2_5() {
+  var myanswer  = "My answer goes here: The tag are not visible because it has the style = display :none. Which will hid the whole tag. But the function doesn't consider the display style." // TODO 2.5: Fill in Answer here
+  document.getElementById("25answer").innerHTML = myanswer; // Do not change
+}
+
+function countDiv() { // Do not add or remove lines to this function
+    var divs = document.getElementsByTagName('div'); // TODO: Question 2.3: Use the document Object to get the number of divs in the HTML page. Replace dummyMethod with the correct one.
+    alert("Number of divs in this page is: " + divs.length); // Do not change 
+}
+
+function makeOrange() { // Do not add or remove lines to this function
+    var narutos = document.getElementsByName('naruto'); // TODO: Question 2.4: Use the document Object to get all tags named "naruto" in the HTML page. Replace dummyMethod with the correct one.
+    for(var i=0; i< narutos.length; i++) { // Do not change
+        narutos[i].style.color = 'orange'; // Do not change
+    }
+}
+
+function getChildrenTags() { // Do not add or remove lines to this function
+  var children = document.getElementById("div_1134").children; // TODO: Question 2.5 Use the appropriate function to get all the children of a div named 'div_1134'
+  var tagNames = ""; // Do not change
+  for (var i = 0; i < children.length; i++) {// Do not change
+    console.log(children[i].tagName);// Do not change
+    tagNames += children[i].tagName +", " ;// Do not change
+  }// Do not change
+  alert(children.length); // Do not change
+  alert("The tags that I found are: " + tagNames);// Do not change
+}
+
